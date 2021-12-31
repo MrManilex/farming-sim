17bde2b48f015be808f8ab441a46ea65ff8480de
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-31 13:29:55 -0500

    create index view for all plants

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
    return HttpResponse('<h1>Testing Testing</h1>')
 
+
 def about(request):
    return render(request, 'about.html')
+
+
+def plants_index(request):
+   return render(request, 'plants/index.html', { 'plants': plants })
+
+
+class Plant: 
+   def __init__(self, name, description, type, price):
+      self.name = name
+      self.description = description
+      self.type = type
+      self.price = price
+
+
+plants = [
+   Plant('Lolo', 'green', 'Kinda rude.', 3),
+   Plant('Sachi', 'hairy', 'Looks like a turtle.', 0),
+   Plant('Fancy', 'blue', 'Happy fluff ball.', 4),
+   Plant('Bonk', 'soft', 'Meows loudly.', 6)
+]
