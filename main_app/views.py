86f59a75b428e1dfd31709558ba4191bc644b21f
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-31 13:14:34 -0500

    stub base html styling

--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,4 @@ def home(request):
    return HttpResponse('<h1>Testing Testing</h1>')
 
 def about(request):
-   return HttpResponse('<h1>This is your ABOUT page</h1>')
+   return render(request, 'about.html')
