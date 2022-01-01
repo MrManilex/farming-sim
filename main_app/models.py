95bf467f7ab0e154c27c6f686a03fddec661a501
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-01 13:32:51 -0500

    create watering model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+   ('M', 'Morning'),
+   ('A', 'Afternoon'),
+   ('E', 'Evening')
+)
+
 
 class Plant(models.Model):
    name = models.CharField(max_length=20)
@@ -13,3 +19,15 @@ class Plant(models.Model):
 
    def get_absolute_url(self):
       return reverse("plants_detail", kwargs={"plant_id": self.id})
+
+
+class Watering(models.Model):
+   date = models.DateField()
+   meal = models.CharField(
+      max_length=1,
+      choices=MEALS,
+      default=MEALS[0][0]
+   )
+
+   def __str__(self):
+      return f'{self.get_meal_display()} on {self.date}'
