87b90ca5c82c9ed1cba369b1a3128d69741ffba1
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-01 12:24:43 -0500

    create details page for plant

--- main_app/views.py
+++ main_app/views.py
@@ -13,5 +13,9 @@ def about(request):
 
 def plants_index(request):
    plants = Plant.objects.all()
-   return render(request, 'plants/index.html', { 'plants': plants })
+   return render(request, 'plants/index.html', {'plants': plants})
 
+
+def plants_detail(request, plant_id):
+   plant = Plant.objects.get(id=plant_id)
+   return render(request, 'plants/detail.html', {'plant': plant})
