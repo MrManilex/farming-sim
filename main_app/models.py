e3918d43e047a69514bd1569514925d9d36f0cec
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-01 12:58:12 -0500

    refactor plant model

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Plant(models.Model):
    name = models.CharField(max_length=20)
    description = models.CharField(max_length=100)
    type = models.CharField(max_length=20)
    price = models.IntegerField()
-   
+
    def __str__(self):
       return self.name
+
+   def get_absolute_url(self):
+      return reverse("plants_detail", kwargs={"plant_id": self.id})
