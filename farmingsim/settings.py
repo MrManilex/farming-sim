9a6f6588622270f2fa32e40afbf496254f5b1614
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-04 13:47:18 -0500

    Config deployment

--- farmingsim/settings.py
+++ farmingsim/settings.py
@@ -128,3 +128,8 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
