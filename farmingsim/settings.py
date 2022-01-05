5c5cfa35f9d81db95d2ce01ae2bacd69e9d4ebb6
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-04 22:46:38 -0500

    refactor

--- farmingsim/settings.py
+++ farmingsim/settings.py
@@ -129,7 +129,7 @@ LOGOUT_REDIRECT_URL = 'home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
